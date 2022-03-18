f1a24de00135bebdee1a90f06974f5bfbba51227
jvarsl <j.varslauskas@gmail.com>
2022-03-18 23:05:41 +0200

    swagger ui distro
    working barebones sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+// npm install swagger-ui-dist --save
+// https://stackoverflow.com/questions/44894013/adding-swagger-ui-to-angular-app
+// https://swagger.io/docs/open-source-tools/swagger-ui/usage/cors/
+
+declare const SwaggerUIBundle: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  constructor(public httpClient: HttpClient) {}
+
   title = 'swagger-ui-test-one';
+
+  private url =
+    'http://rackerlabs.github.io/wadl2swagger/openstack/swagger/dbaas.json';
+
+  async ngOnInit(): Promise<void> {
+    // simulate api call to get json object
+    const res = await this.httpClient
+      .get(this.url, { responseType: 'json' })
+      .toPromise();
+
+    const ui = SwaggerUIBundle({
+      dom_id: '#swagger-ui',
+      layout: 'BaseLayout',
+      presets: [
+        SwaggerUIBundle.presets.apis,
+        SwaggerUIBundle.SwaggerUIStandalonePreset,
+      ],
+      spec: res,
+      // url: 'assets/swagger1.json',
+      // url: 'http://rackerlabs.github.io/wadl2swagger/openstack/swagger/dbaas.json',
+      docExpansion: 'none',
+      operationsSorter: 'alpha',
+    });
+  }
 }
